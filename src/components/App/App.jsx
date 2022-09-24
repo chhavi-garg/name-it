5dcfa450d8a76dcf157f845b53fbd6fa2d821679
Chhavi Garg <chhavu.g@gmail.com>
2022-09-24 15:17:53 +0530

    used props & states

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,14 @@ import './App.css';
 import Header from '../Header/Header';
 
 class App extends React.Component{
+
+    state = {
+        headerText : 'Name It !',
+    };
     render(){
         return(
             <div>
-            <Header />
+            <Header headTitle={this.state.headerText}/>
                 {/* <h1>This is my Class component</h1> */}
             </div>
         )
